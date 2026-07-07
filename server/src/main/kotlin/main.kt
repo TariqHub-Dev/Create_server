81b60708e21d4d162e0dcbb61dd9c4f27b75b54d
Tariq Habibussalim <thabibussalim@gmail.com>
2026-07-07 13:39:15 +0700

    mengubah main untuk deploy

--- server/src/main/kotlin/main.kt
+++ server/src/main/kotlin/main.kt
@@ -1,7 +1,6 @@
 package com.example
 
 import io.ktor.server.engine.*
-import io.ktor.server.application.*
 import io.ktor.server.cio.*
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.SchemaUtils
@@ -9,16 +8,14 @@ import org.jetbrains.exposed.sql.transactions.transaction
 
 fun main(args: Array<String>) {
     Database.connect("jdbc:h2:file:./librarydb;DB_CLOSE_DELAY=-1;", driver = "org.h2.Driver")
-    transaction { 
-        SchemaUtils.create(BookTable) 
+
+    transaction {
+        SchemaUtils.create(BookTable)
     }
 
     val port = System.getenv("PORT")?.toInt() ?: 8080
 
     embeddedServer(CIO, port = port) {
-        configureHttp()
-        configureSerialization()
-        configureResources()
         configureRouting()
     }.start(wait = true)
 }
